ab4c1a34c5fcac7ae7a6afe49cebcada1907cff1
Lucas ALLAIN <92527225+Lacamm@users.noreply.github.com>
2026-02-26 15:16:16 +0100

    US-08 - Route pour la consutation de notes pour l'élève

--- backend/src/main/java/fr/cefim/lespestiferes/gestionpedagogique/core/controllers/UtilisateurController.java
+++ backend/src/main/java/fr/cefim/lespestiferes/gestionpedagogique/core/controllers/UtilisateurController.java
@@ -1,8 +1,10 @@
 package fr.cefim.lespestiferes.gestionpedagogique.core.controllers;
 
 import fr.cefim.lespestiferes.gestionpedagogique.core.entities.Utilisateur;
+import fr.cefim.lespestiferes.gestionpedagogique.core.services.NoteService;
 import fr.cefim.lespestiferes.gestionpedagogique.core.services.UtilisateurService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,9 +13,19 @@ public class UtilisateurController {
     @Autowired
     private UtilisateurService utilisateurService;
 
+    @Autowired
+    private NoteService noteService;
+
     @GetMapping(value = {"/{Id}"})
     public Utilisateur utilisateurParMail(@PathVariable() Integer Id) {
         return utilisateurService.getUtilisateurById(Id);
+    }
+
+    @GetMapping(value = {"/{Id}/notes"})
+    public String notesUtilisateur(Model model, @PathVariable() Integer Id) {
+        model.addAttribute("notes", noteService.getNotesByEleve(Id));
+        model.addAttribute("matieres", noteService.getMatieresByEleves(Id));
+        return "Notes de l'élève avec ses matières ";
     }
 
     @PostMapping(value = {"/creer"})
